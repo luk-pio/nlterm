1b78ad6fb9ac4bb5cfde28de0c81e776a00e0e91
luke.piotrak <luke.piotrak@focusrite.com>
2023-03-31 16:22:24 +0100

    chore: reformat system message

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 const systemMessage = `
 I want you to translate my prompts to terminal commands.
-I will provide you with a prompt. 
-I want you to answer with a command which I can run in the terminal. 
-You should only reply with the terminal command, and nothing else.
+I will provide you with a prompt and I want you to answer with a command which I can run in the terminal. 
+You should only reply with the terminal command and nothing else.
 Do not write explanations. 
-`
+`;
 export const config = {
   openai: {
     apiKey: getEnvVariable("OPENAI_API_KEY"),
